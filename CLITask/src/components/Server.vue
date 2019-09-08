dee70d59a24ebcd3f3497c999d6ba59852b4e21a
Максим Найдович <max.naid@mail.ru>
2019-09-08 15:44:07 +0500

    dive deep into components

---
+++ CLITask/src/components/Server.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="list-group-item">Server # {{index}}
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'Server',
+    props: ['index']
+  }
+</script>
+
+<style scoped>
+  li {
+    border: 1px solid blue;
+  }
+</style>
---
+++ CLITask/src/components/ServerList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="row">
+    <div class="col-xs-12 col-sm-6">
+      <ul class="list-group" v-for="index in 5">
+        <app-server :index="index"></app-server>
+      </ul>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+      <p>Server Details are currently not updated</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Server from "./Server.vue";
+  export default {
+    name: 'ServerList',
+    components: {
+      'app-server': Server
+    }
+  }
+</script>
+
+
