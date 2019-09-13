01f850b5f320c2e795f5350f8537f9ea77e2c2df
Максим Найдович <max.naid@mail.ru>
2019-09-13 17:03:27 +0500

    Custom directives

---
+++ directives/src/main.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.directive('highlight', {
+  //Has 5 methods - hooks: bind, inserted, update, componentUpdated, unbind (bind, update is the most important)
+  bind(el, binding, vnode) {
+    //el.style.backgroundColor = 'green';
+    //el.style.backgroundColor = binding.value;
+
+    let delay = 0;
+    if (binding.modifiers['delayed']) {
+      delay = 3000;
+    }
+
+    setTimeout(() => {
+      if (binding.arg === 'background') {
+        el.style.backgroundColor = binding.value;
+      } else {
+        el.style.color = binding.value;
+      }
+    }, delay);
+
+
+  }
+});
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
