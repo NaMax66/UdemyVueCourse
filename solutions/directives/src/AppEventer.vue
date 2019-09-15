becd1120256095f23acccbbebbce1683eb3a4726
Максим Найдович <max.naid@mail.ru>
2019-09-15 20:56:42 +0500

    Filters

---
+++ solutions/directives/src/AppEventer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="table-bordered">
+    <slot></slot>
+    <button class="btn btn-warning" @click="$emit('some-event', msg)">Eventer</button>
+    <input type="text" v-model="msg">
+    Event production factory
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        msg: 'hey!'
+      }
+    }
+  }
+</script>
