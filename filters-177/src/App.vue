34fdeeb682928a34079c6943cf776bef937fa57a
Максим Найдович <max.naid@mail.ru>
2019-09-16 12:21:00 +0500

    Nice filter realisation

--- filters-177/src/App.vue
+++ filters-177/src/App.vue
@@ -3,7 +3,14 @@
     <div class="row">
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Filters & Mixins</h1>
-        <h2>{{text | toUpperCase}}</h2>
+        <h2>{{text | toUpperCase | to-lowercase }}</h2>
+        <hr>
+        <input type="text" v-model="filterText">
+        <ul style="margin-top: 2rem">
+          <li v-for="fruit in filteredFruits">{{fruit}}</li>
+        </ul>
+        <p v-if="filteredFruits.length === 0"
+           style="color: #bdbdbd">Nothing to show</p>
       </div>
     </div>
   </div>
@@ -13,14 +20,25 @@
   export default {
     data() {
       return {
-        text: 'Hello vue!'
+        text: 'Hello vue!',
+        fruits: ['Apple', 'Banana', 'Mango', 'Melon'],
+        filterText: ''
       }
     },
     filters: {
       toUpperCase(txt){
         return txt.toUpperCase();
       }
+    },
+    computed: {
+      /*nice way to make filter*/
+      filteredFruits() {
+        return this.fruits.filter( el => {
+          return el.match(this.filterText);
+        })
+      }
     }
+
   };
 </script>
 
